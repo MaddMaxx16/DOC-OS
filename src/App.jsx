96c1a808851cf3e365f2b24e49ada1a8e8c6417f
Maxx <Maxx.sharpe16@gmail.com>
2026-08-27 19:45:59 -0400

    Initial DOC OS setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <section className="phone-shell">
+        <div className="status-bar">
+          <span>9:41</span>
+          <span>DOC OS</span>
+        </div>
+
+        <div className="home-screen">
+          <h1>DOC OS</h1>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
