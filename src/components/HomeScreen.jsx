bb7c8649b9a942f88bb48f7c4141e8aa28dbe36a
Maxx <Maxx.sharpe16@gmail.com>
2026-08-27 20:25:54 -0400

    Add Load Board home icon

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -2,6 +2,11 @@ function HomeScreen() {
   return (
     <div className="home-screen">
       <h1>DOC OS</h1>
+
+      <div className="app-icon">
+        <div className="app-icon-square">LB</div>
+        <span>Load Board</span>
+      </div>
     </div>
   )
 }
